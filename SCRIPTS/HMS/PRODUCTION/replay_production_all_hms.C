4024dcc87c4de1ca7f4c5bcbb7571c2c0490e80c
LucasEhinger <LucasEhinger@gmail.com>
2024-08-16 13:18:09 -0400

    Various parameter file edits to (succesfully) run HMS hodo code through LAD hodo decoding.

--- SCRIPTS/HMS/PRODUCTION/replay_production_all_hms.C
+++ SCRIPTS/HMS/PRODUCTION/replay_production_all_hms.C
@@ -22,6 +22,7 @@ void replay_production_all_hms(Int_t RunNumber=0, Int_t MaxEvent=0) {
   pathList.push_back("./raw");
   pathList.push_back("./raw/../raw.copiedtotape");
   pathList.push_back("./cache");
+  pathList.push_back("/cache/hallc/xem2/raw/");
 
   const char* ROOTFileNamePattern = "ROOTfiles/hms_replay_production_all_%d_%d.root";
 
--- SCRIPTS/LAD/PRODUCTION/replay_production_hms_lad.C
+++ SCRIPTS/LAD/PRODUCTION/replay_production_hms_lad.C
@@ -169,7 +169,7 @@ void replay_production_hms_lad(Int_t RunNumber=0, Int_t MaxEvent=0) {
   // Define output ROOT file
   analyzer->SetOutFile(ROOTFileName.Data());
   // Define output DEF-file 
-  analyzer->SetOdefFile("DEF-files/HMS/PRODUCTION/hstackana_production.def");
+  analyzer->SetOdefFile("DEF-files/LAD/PRODUCTION/lstackana_production_all.def");
   // Define cuts file
   analyzer->SetCutFile("DEF-files/HMS/PRODUCTION/CUTS/hstackana_production_cuts.def");    // optional
   // File to record cuts accounting information for cuts
