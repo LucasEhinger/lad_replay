61b42e671ba6fb0904a31774f0975ddb31e4dee5
LucasEhinger <LucasEhinger@gmail.com>
2025-01-16 19:17:45 -0500

    Cosmic and MC replay scrips for LAD hodoscope. Both tested, and functional.

--- SCRIPTS/LAD/SIM/replay_production_lad_mc.C
+++ SCRIPTS/LAD/SIM/replay_production_lad_mc.C
@@ -48,7 +48,7 @@ void replay_production_lad_mc(Int_t RunNumber = 0, Int_t MaxEvent = 0) {
 
   // Load the Hall C detector map
   gHcDetectorMap = new THcDetectorMap();
-  gHcDetectorMap->Load("MAPS/LAD/DETEC/HODO/lhodo.map");
+  gHcDetectorMap->Load("MAPS/LAD/DETEC/HODO/lhodo_mc.map");
 
   // // Add trigger apparatus
   // THaApparatus* TRG = new THcTrigApp("T", "TRG");
